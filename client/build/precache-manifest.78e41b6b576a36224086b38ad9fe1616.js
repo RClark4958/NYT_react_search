7ca294308b40a9a5d4db7661eea18b250e21d814
RCLARK4958 <RICKDCLARK48@GMAIL.COM>
2019-01-29 17:29:46 -0700

    build

---
+++ client/build/precache-manifest.78e41b6b576a36224086b38ad9fe1616.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6731f8a9196532f2b830",
+    "url": "/static/js/main.6731f8a9.chunk.js"
+  },
+  {
+    "revision": "70d5445ab58e6b834b25",
+    "url": "/static/js/1.70d5445a.chunk.js"
+  },
+  {
+    "revision": "6731f8a9196532f2b830",
+    "url": "/static/css/main.c05908f9.chunk.css"
+  },
+  {
+    "revision": "1eb6ebef4a2a970c66df876bba63002f",
+    "url": "/index.html"
+  }
+];
